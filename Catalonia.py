0f13b128e1fbe6384924629865369baa80b6189b
Mateusz Bednarski <xevaquor@hotmail.com>
2015-09-17 18:42:10 +0200

    Refactor + constraint stub

--- Catalonia.py
+++ Catalonia.py
@@ -1,10 +1,23 @@
+#!/usr/bin/python
+# coding=utf-8
+"""
+The Art of an Artificial Intelligence
+http://art-of-ai.com
+https://github.com/artofai
+"""
+
+__author__ = 'xevaquor'
+__license__ = 'MIT'
+
+from collections import namedtuple
+from astar import AStarSolver
+
 Directions = {
     'East': (1, 0),
     'North': (0, -1),
     'South': (0, 1),
     'West': (-1, 0)
 }
-
 
 '''
 - Na piechotę: 10 jednostek czasu, koszt darmowy
@@ -13,40 +26,54 @@ Directions = {
 - Pociąg: 1 jednostka czasu, kosztuje 3 euro
 '''
 
+
 class Tile(object):
     def __int__(self):
         pass
 
     Walk, Bus, Train, Ship = range(4)
 
-
 MoneyCost = {
-    Tile.Walk : 0,
-    Tile.Bus : 2,
-    Tile.Ship : 5,
-    Tile.Train : 3
+    Tile.Walk: 0,
+    Tile.Bus: 2,
+    Tile.Ship: 5,
+    Tile.Train: 3
 }
 
 TimeCost = {
-    Tile.Walk : 10,
-    Tile.Bus : 3,
-    Tile.Ship : 5,
-    Tile.Train : 1
+    Tile.Walk: 10,
+    Tile.Bus: 3,
+    Tile.Ship: 5,
+    Tile.Train: 1
 }
+
+Arrows = {
+    'East': '>',
+    'North': '^',
+    'South': 'v',
+    'West': '<'
+}
+
+TilePrint = {
+    Tile.Bus: 'B',
+    Tile.Ship: 'S',
+    Tile.Walk: ' ',
+    Tile.Train: 'T'
+}
+
+Child = namedtuple("Child", ["state", "move", "back_cost"])
 
 
 # State is simply tuple (x, y)
 
 class Catalonia(object):
-
     def money_cost_func(self, state):
-        x , y = state
+        x, y = state
         tile = self.layout[y][x]
         return MoneyCost[tile]
 
-
     def time_cost_func(self, state):
-        x , y = state
+        x, y = state
         tile = self.layout[y][x]
         return TimeCost[tile]
 
@@ -55,8 +82,8 @@ class Catalonia(object):
         self.expanded = []
         self.shape = (0, 0)
 
-        self.startState = (0,0)
-        self.goalState = (9,9)
+        self.startState = (0, 0)
+        self.goalState = (9, 9)
 
         self.cost_function = lambda s: 0
 
@@ -66,6 +93,7 @@ class Catalonia(object):
 
     def load_from_file(self, filename):
         rows = 0
+        cols = 0
         with open(filename, 'r') as f:
             for line in f:
                 self.layout.append([])
@@ -87,7 +115,7 @@ class Catalonia(object):
                         self.startState = (cols, rows - 1)
                     elif char == 'G':
                         tile = Tile.Walk
-                        self.targetState = (cols, rows - 1)
+                        self.goalState = (cols, rows - 1)
                     elif char == '\r' or char == '\n':
                         break
                     else:
@@ -106,19 +134,33 @@ class Catalonia(object):
             dx, dy = v
             newx = state[0] + dx
             newy = state[1] + dy
+
             if 0 <= newx < self.shape[0] and 0 <= newy < self.shape[1]:
-                yield ((newx, newy),k, self.cost_function((newx, newy)))
+                yield Child((newx, newy),
+                            k,
+                            self.cost_function((newx, newy)))
 
     def is_goal_state(self, state):
         return state == self.goalState
 
 
+def print_solution(layout, solution):
+    s = "\n".join([''.join(map(lambda x: TilePrint[x], row)) for row in layout])
+    rows = s.split('\n')
+    m = [list(x) for x in rows]
+    for i, step in enumerate(solution):
+        x, y = step.previous_state
+        m[y][x] = Arrows[step.step]
+
+    s = '\n'.join([''.join(x) for x in m])
+    print(s)
+
 if __name__ == "__main__":
     c = Catalonia()
+    c.cost_function = c.money_cost_func
     c.load_from_file("input.txt")
+    astar = AStarSolver()
 
-    for l in range(c.shape[1]):
-        row = c.layout[l]
-        print("".join(map(str, row)))
+    result = astar.solve(c)
+    print_solution(c.layout, result)
 
-    print([x for x in c.get_children((1,1))])
--- astar.py
+++ astar.py
@@ -1,150 +1,151 @@
+#!/usr/bin/python
 # coding=utf-8
+"""
+The Art of an Artificial Intelligence
+http://art-of-ai.com
+https://github.com/artofai
+"""
+
+__author__ = 'xevaquor'
+__license__ = 'MIT'
+
 from queue import PriorityQueue
-from Catalonia import *
-from copy import deepcopy
 
 
-class Node:
+class Node(object):
     def __init__(self):
-        self.priority = 0
-        self.parent = None
-        self.state = None
-        self.step = None
-        self.cost = 0
-        self.prevoius_state = None
+        self.priority = 0  # priority for queue
+        self.parent = None  # parent node
+        self.state = None  # state at currnet node
+        self.step = None  # step taken to be here
+        self.cost = 0  # overall cost to be here
+        self.previous_state = None  # state in parent node
         pass
 
+    # required for use in PriorityQueue
     def __lt__(self, other):
         return self.priority < other.priority
 
 
-def find_nearest(node_pos, goal):
-    if len(goal) == 0:
-        return 0
-    return min([taxi(node_pos, x) for x in goal])
+class AStarSolver(object):
+    @staticmethod
+    def taxi(p1, p2):
+        """
+        Computes distance in taxicab (or Manhatan) norm
+        :param p1: First point
+        :param p2: Second point
+        :return: Distance
+        """
+        return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
 
+    @staticmethod
+    def norm_heuristics(node, instance):
+        node_pos = (node[0], node[1])
+        target_pos = (
+            instance.get_target_state()[0], instance.get_target_state()[1])
+        return AStarSolver.taxi(node_pos, target_pos)
 
-def heuristics(node, instance):
-    node_pos = (node[0], node[1])
-    target_pos = (
-    instance.get_target_state()[0], instance.get_target_state()[1])
-    return taxi(node_pos, target_pos)
+    def __init__(self, heuristics_func=None, constraint_func=None):
+        self.heuristics = heuristics_func if heuristics_func is not None else AStarSolver.norm_heuristics
+        self.constraint = constraint_func if constraint_func is not None else lambda state: True
 
+    def solve(self, instance):
+        closed = []  # closed set, will contain expanded nodes in orter to avoid cycles
+        # inicjujemy zbiór otwarty stanem początkowym. Decyzję ustawiamy na null
+        # koszt na 0 - nie ma to znaczenia. Rodzicem jest również null (jest to
+        # korzeń drzewa
+        # fringe dla UCS jest kolejką priorytetową (niższy priorytet powoduje szybsze zdjęcie
+        # elementu
+        # enqueue - put
+        # dequeue - get
+        # fringe (open set) contains nodes that we are currently considering - tehy can be potentially expanded
+        # it is a PriorityQueue to acchieve proper order visitng nodes (lower priority will be taken faster)
+        # we will be adding nodes here to (maybe) use them later
+        fringe = PriorityQueue()
 
-def taxi(p1, p2):
-    return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
+        # initial element in tree is an "empty" node with meaning "we have not taken any decision yet and still stay
+        # in initial state"
 
-def astar(instance, h=heuristics):
-    closed = []  # zbiór zamknięty
-    # inicjujemy zbiór otwarty stanem początkowym. Decyzję ustawiamy na null
-    # koszt na 0 - nie ma to znaczenia. Rodzicem jest również null (jest to
-    # korzeń drzewa
-    # fringe dla UCS jest kolejką priorytetową (niższy priorytet powoduje szybsze zdjęcie
-    # elementu
-    # enqueue - put
-    # dequeue - get
-    fringe = PriorityQueue()
-    # format wierzchołka to:
-    # (priorytet,[(stan, decyzja, koszt), rodzic])
-    # jest to wymagane przez kolejkę.
-    root_node = Node()
-    root_node.parent = None
-    root_node.cost = 0
-    root_node.step = None
-    root_node.state = instance.get_start_state()
-    root_node.priority = 0
-    root_node.prevoius_state = None
-    fringe.put(root_node)
-    # znaleziony cel
-    target = None
+        root_node = Node()
+        root_node.parent = None
+        root_node.cost = 0
+        root_node.step = None
+        root_node.state = instance.get_start_state()
+        root_node.priority = 0
+        root_node.previous_state = None
+        fringe.put(root_node)
 
-    while True:
-        # jeśli zbiór otwarty jest pusty to nie istnieje droga do celu
-        if fringe.empty():
-            return []
-        # pobierz kolejny węzeł z kolejki - ten o najniższym priorytecie
-        # ignorujemy koszt pobrany z kolejki, zamiast niego używamy własności cost węzła
-        node = fringe.get()
-        node_cost = node.cost
+        # found goal state (because there can be may of them we need to remember we have actually found
+        target = None
 
-        # jeśli jesteśmy w stanie docelowym, ustaw cel i zakończ
-        if instance.is_goal_state(node.state):
-            target = node
-            break
+        while True:
+            # if fringe is empty, that meas we already traversed whole tree and have not found soulution
+            # in other words - it is impossible to reach the goal from the initial state. Sorry!
+            if fringe.empty():
+                return []
 
-        # jeśli węzeł nie był rozwijany
-        if node.state not in closed:
-            # dodaj go do zbioru zamkniętego (innymi słowy oznacz jako rozwinięty)
-            closed.append(node.state)
-            # rozwiń go
-            children = instance.get_children(node.state)
+            # get next node from the queue (this one with lowest priority)
+            # remember that in context of A* cost is roughly the same as priority
+            # ignorujemy koszt pobrany z kolejki, zamiast niego używamy własności cost węzła
+            node = fringe.get()
+            node_cost = node.cost
 
-            # print node.state, children
-            # i dla każdego następnika
-            for child in children:
-                child_state, child_step, child_cost = child
-                # dodaj informację o poprzedniku (node jest rodzicem child)
-                # jako koszt ustaw sumę następnika i koszt dojścia do rodzica -
-                # został on odczytany przy rozpakowywaniu krotki zwróconej przez
-                # fringe.get()
-                heuristic_cost = h(child_state,
-                                   instance)  # koszt do wyjścia oszacowany przez heurystykę
-                vertex = Node()
-                vertex.step = child_step
-                vertex.cost = child_cost + node_cost
-                vertex.parent = node
-                vertex.state = child_state
-                vertex.priority = vertex.cost + heuristic_cost
-                vertex.prevoius_state = node.state
-                fringe.put(vertex)
+            # have we found a goal state? if so remember it and get out of loop
+            if instance.is_goal_state(node.state):
+                target = node
+                break
 
-    # lista decyzji prowadzących do rozwiązania
-    solution = []
-    # zaczynamy od węzła z wynikiem
-    i = target
-    # dopóki ma rodzica (nie jesteśmy w korzeniu)
-    while i.parent is not None:
-        # dodaj decyzję która nas tutaj doprowadziła
-        solution.append(i)
-        # przejdź do rodzica
-        i = i.parent
-    # podążaliśmy od wyjścia do startu przez co trzeba odwrócić kolejność
-    solution.reverse()
+            # if current node had not been expanded before, we are allowed to do it now
+            if node.state not in closed:
+                # add it to closed set in order to avoid expanding it once again
+                closed.append(node.state)
+                # expand it
+                children = instance.get_children(node.state)
 
-    return solution
+                # and for each descendant
+                for child in children:
+                    # expand tuple
+                    child_state, child_step, child_cost = child
 
+                    # cost of current node is:
+                    # parent cost + current state cost + heuristics cost
+                    # or
+                    # backward cost + forward cost
 
-arrows = {'East': '>',
-          'North': '^',
-          'South': 'v',
-          'West': '<'}
+                    # compute forward cost by given heuristics
+                    heuristic_cost = self.heuristics(child_state, instance)
 
-TilePrint = {Tile.Bus: 'B',
-             Tile.Ship: 'S',
-             Tile.Walk: ' ',
-             Tile.Train: 'T'
-             }
+                    # and create a new node
 
+                    vertex = Node()
+                    vertex.step = child_step  # step we took to go from parent to this child
+                    vertex.cost = child_cost + node_cost  # *BACKWARD* cost of current node
+                    vertex.parent = node  # parent node
+                    vertex.state = child_state  # state in this node
+                    vertex.priority = vertex.cost + heuristic_cost  # priority is back + forward cost
+                    # it does allow to choose more promising nodes faster
+                    vertex.previous_state = node.state  # state of parent
 
-def print_solution(layout, solution):
-    s = "\n".join([''.join(map(lambda x: TilePrint[x], row)) for row in layout])
+                    # if node violates constraints we are not considering it, and his children
+                    if not self.constraint(vertex):
+                        continue
 
-    rows = s.split('\n')
-    m = [  list(x) for x in rows]
+                    fringe.put(vertex)
 
+        # we got out of loop, that is mean we have sequence of decision used to came to the goal.
+        # now just need to recreate it
 
-    for i, step in enumerate(solution):
-        assert isinstance(step, Node)
-        x, y = step.prevoius_state
-        m[y][x] = arrows[step.step]
+        # sequecne of decisions taken
+        solution = []
+        # starting from goal node
+        i = target
+        # while it does not have a parent (in other words: we are not at the root of decision tree)
+        while i.parent is not None:
+            # add decision taht took us to here
+            solution.append(i)
+            # and process parent node next
+            i = i.parent
+        # we was travelling from goal to start, so just need to reverse.
+        solution.reverse()
 
-    s = '\n'.join([''.join(x) for x in m  ]  )
-    print(s)
-
-if __name__ == "__main__":
-    c = Catalonia()
-    c.cost_function = c.time_cost_func
-    c.load_from_file("input.txt")
-    result = astar(c)
-    print_solution(c.layout, result)
-    print([node.cost for node in result] )
+        return solution
